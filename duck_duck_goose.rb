5e92aac7bbb4460dbc7404cf08b3ada3ccc4f813
Francisco J Trujillo <francisco.jose1982@gmail.com>
2017-10-13 11:26:20 -0500

    adding

--- duck_duck_goose.rb
+++ duck_duck_goose.rb
@@ -1,11 +1,11 @@
 def duck_duck_goose(players, goose)
 
-	0.upto(players.length) {|i| players[i] = {name: players[i]}}
+	0.upto(players.length-1) {|i| players[i] = {name: players[i]}}
 	if players.length < goose
-		return players[goose % players.length-1][:name]
+		return players[(goose % players.length)-1][:name]
 	end
 	players[goose-1][:name]
 end
 
-p duck_duck_goose(["a", "b", "c"], 100000)
+p duck_duck_goose(["a", "b", "c"], 300)
 
